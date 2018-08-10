3ea62fe2f78aae92782280a9b86b5fd4eec6e93b
Timothee MATO <timothee.mato@gmail.com>
2018-08-10 14:07:18 +0200

    install and configure typescript

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 /** @format */
 
-import App from './App';
-import { Navigation } from "react-native-navigation";
+import App from './src/App';
+import { Navigation } from 'react-native-navigation';
 
 Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
-      Navigation.setRoot({
-            root: {
-              component: {
-                    name: "navigation.playground.WelcomeScreen"
-                  }
-            }});
-    });
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
